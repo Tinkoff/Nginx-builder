64e0d61faea22d5a009df4e9f8ba5971b1bdcd56
Maxim Ivanov <m.o.ivanov@tinkoff.ru>
2019-10-02 12:46:31 +0300

    add travis_ci
    fix shell path
    
    fix sh lint
    
    change sh to bash
    
    add pipefail
    
    fix codding
    
    more debug
    
    renew README.md
    
    build only tag
    
    add build status
    
    build only master and tags
    
    add travic CI
    
    add travic CI
    
    add travic CI

---
+++ .travis/deploy_dockerhub.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eux -o pipefail
+
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    TAG="latest"
+else
+    TAG=${TRAVIS_TAG}
+fi
+
+lowercased_TRAVIS_REPO_SLUG=$(echo "${TRAVIS_REPO_SLUG}" | tr '[:upper:]' '[:lower:]')
+lowercased_TAG=$(echo "${TAG}" | tr '[:upper:]' '[:lower:]')
+
+#Publish Centos
+docker build --no-cache -f Dockerfile.centos -t "${lowercased_TRAVIS_REPO_SLUG}:centos-${lowercased_TAG}" .
+docker push "${lowercased_TRAVIS_REPO_SLUG}:centos-${lowercased_TAG}"
+
+#Publish Ubuntu
+docker build --no-cache -f Dockerfile.ubuntu -t "${lowercased_TRAVIS_REPO_SLUG}:ubuntu-${lowercased_TAG}" .
+docker push "${lowercased_TRAVIS_REPO_SLUG}:ubuntu-${lowercased_TAG}"
+
